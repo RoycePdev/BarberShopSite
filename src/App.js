5acb9a1cea9c69e911a1c3b1df1dfc112483d29b
Brian <roycepdev@gmail.com>
2021-11-02 13:23:36 -0700

    beginning React Router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./stylesheets/landing-style.css"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="landing-backdrop">
+      <header>
+        <div className="logo">FadeHouse</div>
+        <nav>
+          <ul>
+            <li>Services</li>
+            <li>Staff</li>
+            <li>Contact</li>
+            <li>Reservations</li>
+          </ul>
+        </nav>
       </header>
+      <div className="landing-info-container">
+        <h1>THIS IS MY HEADER</h1>
+        <p>
+        Tempor nulla eiusmod in sit. Pariatur fugiat eiusmod cillum sit. Ut ipsum minim adipisicing minim minim incididunt et elit deserunt consectetur ad voluptate ullamco. Elit laboris quis aute occaecat id consequat ut aliqua nulla dolor. Ipsum duis aliqua excepteur magna eu veniam aliqua do reprehenderit do sit irure occaecat sint. Enim veniam irure sunt Lorem laborum nulla laboris laborum. Aliquip ex eiusmod occaecat nisi voluptate eu in.
+        </p>
+      </div>
     </div>
   );
 }
---
+++ src/components/Staff.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Staff = () => {
+  return (
+    <div>
+      <h1>Staff</h1>
+    </div>
+  )
+}
