3fb2e9e8d84e8547bae1146818640ef07422f2bc
Brian <roycepdev@gmail.com>
2021-11-05 20:04:15 -0700

    finished map

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,6 +6,9 @@ export const Contact = () => {
     <div>
       <h1>Contact</h1>
       <Map />
+      <div>1052 Garnet Ave, San Diego, CA 92109</div>
+      <div>619-867-5309</div>
+      <div>Open 7 days per week 9am-7pm</div>
     </div>
   )
 }
